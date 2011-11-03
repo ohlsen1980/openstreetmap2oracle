7f2c6ed973cd4de3a43b686010551f9ce6f2253d
brianmayclone_cp <SND\brianmayclone_cp>
2011-11-03 13:17:42 +0000

    + Started implementing Transaction multi threading

---
+++ OpenStreetMap2Oracle/OpenStreetMap2Oracle/businesslogic/Transaction/OSMTransactionObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenStreetMap2Oracle.businesslogic.Transaction
+{
+    public class OSMTransactionObject
+    {
+        public string Query
+        {
+            get;
+            set;
+        }
+
+        public OSMTransactionObject(string query) {
+            this.Query = query;
+        }
+    }
+}
