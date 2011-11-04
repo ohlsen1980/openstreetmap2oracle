e02ae2027d64c88858503b17f0cf48b6553dadcd
brianmayclone_cp <SND\brianmayclone_cp>
2011-11-04 23:11:28 +0000

    + Created new MainWindow2 + Added ProgressWindow + Redesigned the whole UI + Created AppManagerController for Business logics, so our Windows don't contain any business logic anymore + Added a new UserControl HugeButton for use in our Main Window

--- OpenStreetMap2Oracle/OpenStreetMap2Oracle/MainWindow2.xaml.cs
+++ OpenStreetMap2Oracle/OpenStreetMap2Oracle/MainWindow2.xaml.cs
@@ -9,6 +9,11 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using OpenStreetMap2Oracle.gui;
+using Microsoft.Win32;
+using OpenStreetMap2Oracle.tools;
+using System.IO;
+using OpenStreetMap2Oracle.controller;
 
 namespace OpenStreetMap2Oracle
 {
@@ -23,5 +28,37 @@ namespace OpenStreetMap2Oracle
 			
 			// Fügen Sie Code, der bei der Objekterstellung erforderlich ist, unter diesem Punkt ein.
 		}
+
+		private void btnCreateDBConnection(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+            DbLoginWindow dbDialog = new DbLoginWindow(this);
+            if (dbDialog.ShowDialog() == true)
+            {
+                btnSelectFile.Disabled = false;
+            }
+		}
+
+		private void btnSelectFile_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "OpenStreetMap Dateien (*.osm)|*.OSM|" +
+                            "Alle Dateien (*.*)|*.*";
+            dialog.InitialDirectory = PathProvider.Path;
+
+            if (dialog.ShowDialog() == true)
+            {
+                PathProvider.Path = new FileInfo(dialog.FileName).DirectoryName;
+                btnStartMigration.Disabled = false;
+            }
+			
+		}
+
+        private void btnStartMigration_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.IsEnabled = false;
+
+            AppManagerController controller = new AppManagerController();
+            controller.Start();
+        }
 	}
 }
