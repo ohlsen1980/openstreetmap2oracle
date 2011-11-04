e22a27674d590a9a50c1fc6eef7898cfdf3c7bbd
brianmayclone_cp <SND\brianmayclone_cp>
2011-11-04 06:47:39 +0000

    + Added comments + Made the TransactionQueue a Collection

--- OpenStreetMap2Oracle/OpenStreetMap2Oracle/businesslogic/Transaction/OSMTransactionObject.cs
+++ OpenStreetMap2Oracle/OpenStreetMap2Oracle/businesslogic/Transaction/OSMTransactionObject.cs
@@ -1,18 +1,49 @@
-﻿using System;
+﻿/*
+ OpenStreetMap2Oracle - A windows application to export OpenStreetMap Data 
+                (*.osm - files) to an oracle database
+ -------------------------------------------------------------------------------
+ Copyright (C) 2011  Christian Möller
+ -------------------------------------------------------------------------------
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace OpenStreetMap2Oracle.businesslogic.Transaction
 {
+    /// <summary>
+    /// Object for transaction purposes
+    /// </summary>
     public class OSMTransactionObject
     {
+        /// <summary>
+        /// Gets or sets the query string
+        /// </summary>
         public string Query
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Creates a new instance of OSMTransactionObject
+        /// </summary>
+        /// <param name="query">Query string</param>
         public OSMTransactionObject(string query) {
             this.Query = query;
         }
