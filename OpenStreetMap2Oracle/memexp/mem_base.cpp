b6d51e3dee90955a61a15c9d083e6f6f95bd8708
brianmayclone_cp <SND\brianmayclone_cp>
2011-11-10 08:25:17 +0000

    + implemented PUTA (multiple items add) with COMMIT

--- OpenStreetMap2Oracle/memexp/mem_base.cpp
+++ OpenStreetMap2Oracle/memexp/mem_base.cpp
@@ -1,26 +1,40 @@
+/* 
+  memexp - A x64 windows console application for extending the capabilities  
+                 of .NET CLR apps suffering from 2GB memory limit
+  -------------------------------------------------------------------------------
+  Copyright (C) 2011  Christian Möller
+  -------------------------------------------------------------------------------
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 #include "StdAfx.h"
 #include "mem_base.h"
-
-
 
 mem_base::mem_base(void)
 {
 	
 }
 
-
-
 mem_base::~mem_base(void)
 {
 	free(&_data);
 }
 
-
-
 void mem_base::mem_base_set(long _id, char *point) {
 	_data[_id] = point;
 }
-
 
 char *mem_base::mem_base_get(long _id) {
 	if (_data.count(_id) > 0) {
--- OpenStreetMap2Oracle/memexp/memexp.cpp
+++ OpenStreetMap2Oracle/memexp/memexp.cpp
@@ -1,12 +1,50 @@
-// memexp.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
-//
+/* 
+  memexp - A x64 windows console application for extending the capabilities  
+                 of .NET CLR apps suffering from 2GB memory limit
+  -------------------------------------------------------------------------------
+  Copyright (C) 2011  Christian Möller
+  -------------------------------------------------------------------------------
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 
 #include "stdafx.h"
-#include "mem_base.h"
-#include <cstdio>
-#include <iostream>
+
+#define COMMAND_PUT		"PUT"
+#define COMMAND_PUTA	"PUTA"
+#define COMMAND_COMMIT	"COMMIT"
+#define COMMAND_GET		"GET"
+#define COMMAND_EXIT	"EXIT"
+#define COMMAND_COUNT	"COUNT"
+#define COMMAND_CLEAR	"CLEAR"
 
 using namespace std;
+
+long _cin_PUT(char* buffer_id, char* buffer_data) {
+	long _id;
+
+	cin >> buffer_id;
+
+	if (strcmp(buffer_id, COMMAND_COMMIT) == 0) {
+		return 0;
+	}
+
+	cin >> buffer_data;
+
+	_id = atol(buffer_id);
+	return _id;
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -20,16 +58,25 @@ int _tmain(int argc, _TCHAR* argv[])
 	while (true) {
 		cin >> buffer_cmd;
 		
-		if (strcmp(buffer_cmd, "PUT") == 0) {
-			cin >> buffer_id;
-			cin >> buffer_data;
-			_id = atol(buffer_id);
-			mbase->mem_base_set(_id, buffer_data);
-		} else if (strcmp(buffer_cmd, "GET") == 0) {
-			cin >> buffer_id;
-			_id = atol(buffer_id);
-			cout << mbase->mem_base_get(_id) << endl;
-		} else if (strcmp(buffer_cmd, "EXIT") == 0) {
+		if (buffer_cmd != NULL) {
+			if (strcmp(buffer_cmd, COMMAND_PUT) == 0) {
+				_id = _cin_PUT(buffer_id, buffer_data);
+				mbase->mem_base_set(_id, buffer_data);
+			} else if (strcmp(buffer_cmd, COMMAND_PUTA) == 0) {
+				while ((_id = _cin_PUT(buffer_id, buffer_data)) > 0) {
+					mbase->mem_base_set(_id, buffer_data);
+				} 
+			} else if (strcmp(buffer_cmd, COMMAND_GET) == 0) {
+				cin >> buffer_id;
+				_id = atol(buffer_id);
+				cout << mbase->mem_base_get(_id) << endl;
+			} else if (strcmp(buffer_cmd, COMMAND_EXIT) == 0) {
+				exit(0);
+			} else if (strcmp(buffer_cmd, COMMAND_COUNT) == 0) {
+				
+			} else if (strcmp(buffer_cmd, COMMAND_CLEAR) == 0) {
+			} 
+		} else {
 			exit(0);
 		}
 	}
